eaad475b97aebfd493ed61f16df9d984e1da7f25
Your Name <basheer.80in@gmail.com>
2021-10-23 23:01:25 +0530

    first github project

---
+++ src/org/day10/Day10Class.java
@@ -0,0 +1,30 @@
+package org.day10;
+
+import java.util.Scanner;
+public class Day10Class {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("enter your age = ");
+		
+		byte b = sc.nextByte();
+		
+		System.out.println("the age is " +b );
+		
+		System.out.println("\nenter your otp:");
+		short ns = sc.nextShort();
+			System.out.println(ns);
+			
+			
+			int ni = sc.nextInt();
+			System.out.println(ni);
+	}
+
+	
+	
+			
+	}
+
